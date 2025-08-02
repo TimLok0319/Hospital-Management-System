ae78eb972754139ddfb4e3e69ef31636a4380c8f
TimLok0319 <timlok0319@gmail.com>
2025-08-03 00:39:48 +0800

    Modify scanner
    Added scanner for input reading

--- src/Staff.java
+++ src/Staff.java
@@ -4,10 +4,9 @@ public class Staff {
 	private String id, name, designation, sex;
 	private int salary;
 	
-	Scanner read = new Scanner(System.in);
 	
-	public void newStaff()
-	{
+	public void newStaff(Scanner read)
+	{	
 		System.out.println("----- New Staff Registration -----");
 		System.out.print("Staff's ID: ");
 		id = read.nextLine();
