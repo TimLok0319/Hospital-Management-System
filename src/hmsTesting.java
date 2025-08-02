ede03f8fc5881d0e09beb3c281c02da46decb0f7
TimLok0319 <timlok0319@gmail.com>
2025-08-03 00:40:40 +0800

    Testing Changes

--- src/hmsTesting.java
+++ src/hmsTesting.java
@@ -1,4 +1,9 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+import java.util.Scanner;
+
+>>>>>>> Stashed changes
 =======
 import java.util.Scanner;
 
@@ -8,13 +13,19 @@ public class hmsTesting {
 	public static void main(String[] args) {
 		
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 		Staff staff = new Staff();
 		staff.newStaff();
 =======
+=======
+>>>>>>> Stashed changes
 		Scanner read = new Scanner(System.in);
 		
 		Staff staff = new Staff();
 		staff.newStaff(read);
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 		
 		staff.showStaffInfo();
