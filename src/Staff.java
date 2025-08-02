4b986c4d9ec9e0b7e27c649b43ddfa1ccbfbf7b0
TimLok0319 <timlok0319@gmail.com>
2025-08-02 17:02:18 +0800

    Added Staff Class

---
+++ src/Staff.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Staff {
+	private String id, name, designation, sex;
+	private int salary;
+	
+	Scanner read = new Scanner(System.in);
+	
+	public void newStaff()
+	{
+		System.out.println("----- New Staff Registration -----");
+		System.out.print("Staff's ID: ");
+		id = read.nextLine();
+		System.out.print("Name: ");
+		name = read.nextLine();	
+		System.out.print("Designation: ");
+		designation = read.nextLine();	
+		System.out.print("Sex: ");
+		sex = read.nextLine();	
+		System.out.print("Salary: ");
+		salary = read.nextInt();	
+		read.nextLine();
+	}
+	
+	public void showStaffInfo()
+	{
+		System.out.println("["+id+"]"+"    ["+name+"]    ["+designation+"]    ["+sex+"]    ["+salary+"]");
+	}
+}
