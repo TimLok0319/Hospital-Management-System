305cc067ffb4d8446b2d97fe20bfdadf5a916f08
TimLok0319 <timlok0319@gmail.com>
2025-09-10 00:47:30 +0800

    Create HospitalManagementGUI.java

---
+++ src/HospitalManagementGUI.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+
+public class HospitalManagementGUI extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
